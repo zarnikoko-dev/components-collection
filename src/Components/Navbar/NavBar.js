f603b1539b95a9e4ab0faf5b7ea0b1e6e81ab8d5
Zar Ni Ko Ko <zarnikoko.ucsy@gmail.com>
2021-06-02 18:54:12 +0630

    NavBar
    with transform translate

---
+++ src/Components/Navbar/NavBar.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import "./NavBar.css";
+import logo from '../../images/logo.png'
+
+const NavBar = (props) => {
+    const [slide,setSlide] = useState(false);
+    return(
+        <nav className="nav">
+            <div className="menu-logo" onClick={()=>setSlide(prev=>!prev)}>
+                <div className="menu">
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+                <div className="logo-text">LOGO</div>
+            </div>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Help</a></li>
+            </ul>
+            <div></div>
+            <ul className={"sidebar"+(slide?' active':'')}>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Help</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
